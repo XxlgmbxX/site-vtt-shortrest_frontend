7c639f02c0d1afeba007a86b29d1646a65c04533
XxlgmbxX <luizbiel222@gmail.com>
2024-09-01 00:06:16 -0300

    sexto commit

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 const login = document.querySelector(".login");
 const loginForm = document.querySelector(".loginForm");
 const loginInput = document.querySelector(".loginInput");
+
 // Chat sender elements
 const sender = document.querySelector(".sender");  // Corrigido de ".login" para ".sender"
 const senderForm = document.querySelector(".senderForm");
 const senderInput = document.querySelector(".senderInput");
 const messagesArea = document.querySelector(".messagesArea");
 const textarea = document.querySelector('.sender textarea');
+
 textarea.addEventListener('keydown', function(event) {
     if (event.keyCode === 13 && !event.shiftKey) {
         event.preventDefault();
@@ -18,34 +20,45 @@ textarea.addEventListener('keydown', function(event) {
         }
     }
 });
+
 const user = {id: "", name: ""};
 let websocket;
+
 const createMessage = (userName, content) => {
     const div = document.createElement("div");
     const span = document.createElement("span");
     const lineBreak = document.createElement("br");
     const text = document.createElement("p");
+
     div.classList.add("messages");
     span.classList.add("userSender");
+
     div.appendChild(span);
     div.appendChild(lineBreak);
     div.appendChild(text);
+
     span.innerHTML = userName;
     text.innerHTML = content;
+
     return div;
 };
+
 const createRoll = (userName, content) => {
     const div = document.createElement("div");
     const span = document.createElement("span");
     const lineBreak = document.createElement("br");
     const roll = document.createElement("p");
+
     div.classList.add("messages");
     span.classList.add("userSender");
+
     div.appendChild(span);
     div.appendChild(lineBreak);
     div.appendChild(roll);
+
     span.innerHTML = userName
     roll.innerHTML = content
+
     return div;
 };
 const scrollScreen = () => {
@@ -55,11 +68,11 @@ const scrollScreen = () => {
 const processMessage = ({ data }) => {
     const { type, userId, userName, content} = JSON.parse(data);
     let element;
+
     if (type === "roll") {
         element = createRoll(userName, content);
     } 
     if (type === "message") {
-        element = createMessage(content, userName);
         element = createMessage(userName, content);
     }
 
@@ -71,20 +84,24 @@ const handleLogin = (event) => {
     user.id = crypto.randomUUID();
     user.name = loginInput.value;
     login.style.display = "none";
+
     websocket = new WebSocket("wss://site-teste-kawk.onrender.com");
     websocket.onmessage = processMessage;
+
     websocket.onerror = function(error) {
         console.error("Erro ao conectar ao servidor WebSocket:", error);
     };
 };
 const sendMessage = (messageContent) => {
     event.preventDefault();
+
     const message = {
         userId: user.id,
         userName: user.name,
         content: messageContent,
         type: "message"
     };
+
     websocket.send(JSON.stringify(message));
     textarea.value = "";
 };
@@ -96,6 +113,7 @@ const sendRoll = (dice) => {
         content: Math.floor(Math.random() * dice) + 1,
         type: "roll"
     }
+
     websocket.send(JSON.stringify(roll));
 }
 loginForm.addEventListener("submit", handleLogin);
